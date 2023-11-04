03cfd6ded13326c9f04275ebb055e18ef9d99e77
eerri <eerriikk@student.ubc.ca>
2023-11-04 13:10:05 -0700

    initial commit

---
+++ pani_puri/src/file_loader.py
@@ -0,0 +1,10 @@
+import os
+
+from constants import VECTORDB_FILE_PATH
+from getfiles import CanvasDownloader
+
+CANVAS_API_KEY = os.environ.get("CANVAS_API_KEY")
+CANVAS_API_URL = "canvas.ubc.ca"
+
+API = CanvasDownloader(CANVAS_API_URL, CANVAS_API_KEY, VECTORDB_FILE_PATH)
+API.download_files(True, "both")
