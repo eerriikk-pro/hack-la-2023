3b84421098307141caf8b8598763b0040704a69e
eerri <eerriikk@student.ubc.ca>
2023-11-04 13:27:32 -0700

    Ignore pdfs

--- pani_puri/src/file_loader.py
+++ pani_puri/src/file_loader.py
@@ -7,4 +7,4 @@ CANVAS_API_KEY = os.environ.get("CANVAS_API_KEY")
 CANVAS_API_URL = "canvas.ubc.ca"
 
 API = CanvasDownloader(CANVAS_API_URL, CANVAS_API_KEY, VECTORDB_FILE_PATH)
-API.download_files(True, "both")
+API.download_files(False, "both")
